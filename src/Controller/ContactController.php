6cf3e3e086417b0a3d215f3e489e8b25bb0914ab
lolokokoo <mano.raichon@etu.univ-amu.fr>
2023-02-20 19:52:40 +0100

    emailservice

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,18 +4,17 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactType;
+use App\Service\MailService;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact.index')]
-    public function index(Request $request, EntityManagerInterface $manager, MailerInterface $mailer): Response
+    public function index(Request $request, EntityManagerInterface $manager, MailService $mailService): Response
     {
         $contact = new Contact();
         if ($this->getUser()){
@@ -31,18 +30,12 @@ class ContactController extends AbstractController
             $manager->flush();
 
             //Envoie de l'email (en local => mailtrap.io)
-            $email = (new TemplatedEmail())
-                ->from($contact->getEmail())
-                ->to('admin@simrecipe.com')
-                ->subject($contact->getSubject())
-                // path of the Twig template to render
-                ->htmlTemplate('emails/contact.html.twig')
-                // pass variables (name => value) to the template
-                ->context([
-                    'contact' => $contact
-                ]);
-
-            $mailer->send($email);
+            $mailService->sendEmail(
+                $contact->getEmail(),
+                $contact->getSubject(),
+                'emails/contact.html.twig',
+                ['contact' => $contact]
+            );
 
             $this->addFlash(
                 'success',
